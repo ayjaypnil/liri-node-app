bec54319eea23734bc33946ea5054304ae782ec9
Anil Patel <ajpatel92@icloud.com>
2018-03-01 12:45:40 -0600

    file stuff

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
 
 console.log("this is loaded");
 
--- liri.js
+++ liri.js
@@ -1,1 +1,8 @@
 require("dotenv").config();
+
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
